6fd2ab0edb64721a3865ad49f3f9fc03a4a100f4
Rachel Prehodka-Spindel <rachelps1125@gmail.com>
2016-07-19 18:09:04 -0400

    Added Checkbox Counter

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 	$('.list-items').html(localStorage.my_list);
 	var totalItems = localStorage.total_items || 0;
 	var boughtItems = 0;
-	updateCounter();
+	updateCounterTotal();
 	var addItem = function(){
 		var item = $('input').val();
 		$('.list-items').prepend('<li class="list"><input class="check" type="checkbox"/>'+item+'<button class="delete-button">Delete</button></li>');
 		$('.text').val('');
 		totalItems = totalItems+1;
-		updateCounter();
+		updateCounterTotal();
 		saveToLocal();
 	}
 	$('.add').click(addItem);
@@ -20,19 +20,26 @@ $(document).ready(function(){
 
 	$('.list-items').on('click', '.check', function () {
 		$(this).parent().toggleClass('checked');
+		updateCounterBought();
 		saveToLocal();
 	})
 	$('.list-items').on('click', '.delete-button', function () {
 		$(this).parent().remove();
 		totalItems = totalItems-1;
-		updateCounter(); 
+		updateCounterTotal(); 
+		updateCounterBought();
 		saveToLocal();
 	})
 	var saveToLocal = function(){
 		localStorage.my_list = $('.list-items').html();
 		localStorage.total_items = totalItems;
 	}
-	function updateCounter(){
+	function updateCounterTotal(){
 		$('.total-items').text(totalItems);
 	}
+	function updateCounterBought(){
+		boughtItems = $(':checkbox:checked').length;
+		$('.bought-items').text(boughtItems);
+	}
+
 });
